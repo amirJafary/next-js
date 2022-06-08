dc140851c75483541938454e600ce2e17b958cb7
a.h.jafari <amirjaffari300245@gmail.com>
2022-06-08 09:39:17 +0430

    setup project(shop) / nextJs with tailwindCss

---
+++ pages/aboutUs/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            <strong>hey !</strong>
+            <div>i'm amir</div>
+            <div>
+                my first job is civil engineering & my second job is react developer
+            </div>
+
+        </div>
+    );
+};
+
+export default index;
---
+++ pages/contactUs/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            <h1>contact Us</h1>
+
+            <div>
+                phone number: +98 936 4500972
+            </div>
+            <div>
+                email: amirjafari@gmail.com
+            </div>
+            <div>
+                address: iran - tehran - shahriyar - no 109
+            </div>
+        </div>
+    );
+};
+
+export default index;
